fd99896f3b440e20a54d09a49ef40779025b4e7f
Arman Ali <07arman2004@gmail.com>
2025-05-25 12:55:56 +0000

    Public and private documents should be on half of the screen left side should be private documents and left side should be public documents

--- src/app/dashboard/documents/page.tsx
+++ src/app/dashboard/documents/page.tsx
@@ -57,7 +57,7 @@ export default function DocumentsPage() {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div className="grid grid-cols-2 gap-8"> {/* Changed from grid-cols-1 md:grid-cols-2 */}
         {/* Private Documents Column */}
         <Card className="shadow-lg rounded-xl border-border/50">
           <CardHeader>
