29b55f7e4a8793feebb54a02ee8ed6517c8bc515
Arman Ali <07arman2004@gmail.com>
2025-09-17 06:38:38 +0000

    Provide me path of new meeting page in text format not in file format or

--- src/app/dashboard/start-meeting/page.tsx
+++
@@ -1,9 +0,0 @@
-
-import { redirect } from 'next/navigation';
-
-// This page is deprecated as the "Start New Meeting" functionality
-// has been moved into a dialog accessible from the homepage and sidebar.
-// This component redirects users to the homepage.
-export default function DeprecatedStartMeetingPage() {
-  redirect('/');
-}
