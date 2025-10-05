2f7d82835b24d4942b29762b3aec1a1ada355e1f
Arman Ali <07arman2004@gmail.com>
2025-10-05 06:10:15 +0000

    Again app is taking lot of time to start make it start fast

--- next.config.ts
+++ next.config.ts
@@ -13,12 +13,41 @@ const nextConfig = {
   //   ],
   // },
   experimental: {
-    optimizePackageImports: ['lucide-react'],
+    serverActions: true,
+    instrumentationHook: true,
+    optimizePackageImports: [
+      'lucide-react',
+      '@radix-ui/react-accordion',
+      '@radix-ui/react-alert-dialog',
+      '@radix-ui/react-avatar',
+      '@radix-ui/react-checkbox',
+      '@radix-ui/react-dialog',
+      '@radix-ui/react-dropdown-menu',
+      '@radix-ui/react-label',
+      '@radix-ui/react-menubar',
+      '@radix-ui/react-popover',
+      '@radix-ui/react-progress',
+      '@radix-ui/react-radio-group',
+      '@radix-ui/react-scroll-area',
+      '@radix-ui/react-select',
+      '@radix-ui/react-separator',
+      '@radix-ui/react-slider',
+      '@radix-ui/react-slot',
+      '@radix-ui/react-switch',
+      '@radix-ui/react-tabs',
+      '@radix-ui/react-toast',
+      '@radix-ui/react-tooltip',
+      'firebase/auth',
+      'firebase/firestore',
+      'firebase/storage',
+      '@genkit-ai/flow',
+      '@genkit-ai/googleai',
+    ],
     fontLoaders: [
       { loader: '@next/font/google', options: { subsets: ['latin'] } },
     ],
   },
 };
 
-// Incrementing this comment to force a cache clear: 9
+// Incrementing this comment to force a cache clear: 11
 module.exports = nextConfig;
