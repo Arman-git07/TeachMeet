346f44c1b2bbca89865225459c61131438a6b337
Arman-git07 <07arman2004@gmail.com>
2026-03-14 12:07:12 +0530

    git commit -m "fix: separate ControlButton component and clean CameraToggle"

--- src/components/CameraToggle.tsx
+++ src/components/CameraToggle.tsx
@@ -1,26 +1,9 @@
-
 "use client";
 
 import React from "react";
 import { Video, VideoOff } from "lucide-react";
 import { cn } from "@/lib/utils";
 import ControlButton from "@/components/ControlButton";
-import React from "react";
-import { cn } from "@/lib/utils";
-
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
-
-export default function ControlButton({ className, ...props }: Props) {
-  return (
-    <button
-      className={cn(
-        "flex items-center justify-center rounded-full p-3 bg-gray-800 hover:bg-gray-700 text-white",
-        className
-      )}
-      {...props}
-    />
-  );
-}
 
 type CameraToggleProps = {
   isCameraOn: boolean;
@@ -28,18 +11,27 @@ type CameraToggleProps = {
   className?: string;
 };
 
-const CameraToggle = React.forwardRef<HTMLButtonElement, CameraToggleProps>(({ isCameraOn, onToggle, className }, ref) => {
-  return (
-    <ControlButton
+const CameraToggle = React.forwardRef<HTMLButtonElement, CameraToggleProps>(
+  ({ isCameraOn, onToggle, className }, ref) => {
+    return (
+      <ControlButton
         label={isCameraOn ? "Stop Camera" : "Start Camera"}
         onClick={onToggle}
-        className={cn(!isCameraOn && "bg-destructive hover:bg-destructive/90", className)}
+        className={cn(
+          !isCameraOn && "bg-destructive hover:bg-destructive/90",
+          className
+        )}
         ref={ref}
       >
-      {isCameraOn ? <Video className="h-6 w-6" /> : <VideoOff className="h-6 w-6" />}
-    </ControlButton>
-  );
-});
+        {isCameraOn ? (
+          <Video className="h-6 w-6" />
+        ) : (
+          <VideoOff className="h-6 w-6" />
+        )}
+      </ControlButton>
+    );
+  }
+);
 
 CameraToggle.displayName = "CameraToggle";
 
