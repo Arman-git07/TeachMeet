0947942061721a238c841c4fa59d95cabe7da36c
Arman-git07 <07arman2004@gmail.com>
2026-03-30 18:00:12 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,17 @@ import { useEffect } from "react";
 export default function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then((reg) => console.log("SW registered:", reg))
-        .catch((err) => console.log("SW failed:", err));
-    }
-  }, []);
+  console.log("Trying to register SW...");
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((reg) => console.log("✅ SW registered:", reg))
+      .catch((err) => console.log("❌ SW failed:", err));
+  } else {
+    console.log("❌ Service Worker not supported");
+  }
+}, []);
 
   return <Component {...pageProps} />;
 }
