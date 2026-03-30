52b98dbe05fc42945a7c8c224b40c424bab62d64
Arman-git07 <07arman2004@gmail.com>
2026-03-30 19:29:42 +0530

    git add . git commit -m "Fix service worker logic" git push

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,12 @@
 self.addEventListener("install", (event) => {
-  console.log("Service Worker installing.");
+  console.log("Service Worker installing...");
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("Service Worker activated.");
+  console.log("Service Worker activating...");
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(fetch(event.request));
+  // Basic fetch handler (required)
 });
