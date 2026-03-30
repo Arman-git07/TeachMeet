a2a922b03add0712242c717e52c842d3f7a2a026
Arman-git07 <07arman2004@gmail.com>
2026-03-30 09:25:46 +0530

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,10 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" href="/manifest.json" />
-<meta name="theme-color" content="#0f172a" />
-<link rel="apple-touch-icon" href="/icon-192.png" />
-      </Head>
+  <link rel="manifest" href="/manifest.json" />
+  <meta name="theme-color" content="#0f172a" />
+  <link rel="apple-touch-icon" href="/icon-192.png" />
+  <link rel="icon" href="/favicon.ico" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+</Head>
       <body>
         <Main />
         <NextScript />
