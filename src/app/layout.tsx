a84769e2682245cd46363688de7717697835b6b6
Arman-git07 <07arman2004@gmail.com>
2026-03-31 13:14:20 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
         <link rel="apple-touch-icon" href="/icon-192.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0f172a" />
-
+<meta name="mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
         {/* ✅ EARLY SW REGISTRATION */}
         <script dangerouslySetInnerHTML={{
           __html: `
