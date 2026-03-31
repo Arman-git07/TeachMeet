2befbd1119928ca00889e96b9fedf4cfe9575670
Arman-git07 <07arman2004@gmail.com>
2026-03-31 13:03:34 +0530

    Create SWRegister.tsx

---
+++ src/components/SWRegister.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function SWRegister() {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then((reg) => {
+          console.log("✅ Service Worker Registered", reg);
+        })
+        .catch((err) => {
+          console.error("❌ Service Worker Error", err);
+        });
+    }
+  }, []);
+
+  return null;
+}
